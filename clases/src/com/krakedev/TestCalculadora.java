fda6350c9f1dea5003fb78f155adc2bd289e9ae1
2Erik <erik19yogo@gmail.com>
2026-01-21 21:58:53 -0500

    Llamado de las funciones de la calculadora

--- clases/src/com/krakedev/TestCalculadora.java
+++ clases/src/com/krakedev/TestCalculadora.java
@@ -6,15 +6,27 @@ public class TestCalculadora {
 		Calculadora calcu;
 		int resultadoSuma;
 		int resultadoResta;
+		double resultadoMultiplicar;
+		double resultadoDividir;
+		double resultadoPromedio;
 		
 		calcu = new Calculadora();
+		
 		calcu.suma(5, 8);
 		resultadoSuma = calcu.suma(8, 3);
 		resultadoResta = calcu.resta(5, 2);
+		resultadoMultiplicar = calcu.multiplicar(2.4, 2);
+		resultadoDividir = calcu.dividir(7, 3);
+		resultadoPromedio = calcu.promedio(8, 3, 8);
+		calcu.mostarResultado();
+		
 		
 		
 		System.out.println("RESULTADO SUMA: "+resultadoSuma);
 		System.out.println("RESULTADO RESTA: "+resultadoResta);
+		System.out.println("RESULTADO MULTIPLICACION: "+resultadoMultiplicar);
+		System.out.println("RESULTADO DIVIDIR: "+resultadoDividir);
+		System.out.println("RESULTADO PROMEDIO: "+resultadoPromedio);
 	}
 
 }
