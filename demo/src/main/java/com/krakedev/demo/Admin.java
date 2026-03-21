2ad4dc65e8d4fc0760f0c72618f801fdc5391e7d
2Erik <erik19yogo@gmail.com>
2026-03-21 15:08:22 -0500

    Tero 30 terminado

---
+++ demo/src/main/java/com/krakedev/demo/Admin.java
@@ -0,0 +1,16 @@
+package com.krakedev.demo;
+
+import org.apache.logging.log4j.Logger;
+
+public class Admin {
+	private Logger logger=org.apache.logging.log4j.LogManager.getLogger(Admin.class);
+	
+	public void agregar() {
+		 logger.info("mensaje de info");
+		 logger.warn("mensaje de warn");
+		 logger.trace("mensaje de trace");
+		 logger.error("mensaje de error");
+		 logger.debug("mensaje de debug");
+	}
+	
+}
